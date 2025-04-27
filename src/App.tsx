2f79d5aad83045d6a79f4a0ec41d3e0d7582d9da
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2025-04-26 22:50:00 -0400

    chore: setup initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div class="App">
+      <h2>TMDB Movies</h2>
+    </div>
+  
+  )
+}
+
+export default App
