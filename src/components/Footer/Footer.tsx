6aa0b28881ff0ab0a69cebb21128a61e1b55d3f6
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2025-04-28 22:32:10 -0400

    feat: create inital footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function Footer() {
+    return (
+      <footer className="footer">
+        © 2025 Movies App. Todos os direitos reservados.
+      </footer>
+    );
+  }
+  
+  export default Footer;
