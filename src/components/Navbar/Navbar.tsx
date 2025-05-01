1158c34aa150cb924b5ae355e5e8bb77816e5168
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2025-05-01 00:02:48 -0400

    fix: correct navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,34 +1,32 @@
-import './index.css'
-
-import { Link } from 'react-router-dom';
+import "./index.css";
+import { GiClapperboard } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar">
-
-    <div className="logoSection">
-        <img src="/logo.png" alt="Logo" className="logo" /> 
+      <div className="logoSection">
+        <GiClapperboard className="logo" />
         <span className="siteName">Movies 2024</span>
       </div>
 
       <ul className="navLinks">
         <li>
-          <Link to="/" className="link">Home</Link>
+          <Link to="/" className="link">
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/movies" className="link">Movies</Link>
+          <Link to="/movies" className="link">
+            Movies
+          </Link>
         </li>
         <li>
-          <Link to="/about" className="link">About</Link>
-        </li>
-        <li>
-          <Link to="/contact" className="link">Contact</Link>
+          <Link to="/about" className="link">
+            About
+          </Link>
         </li>
       </ul>
-
-      <div className="searchBox">
-        <input type="text" placeholder='Search'/>
-      </div>
     </nav>
   );
 }
