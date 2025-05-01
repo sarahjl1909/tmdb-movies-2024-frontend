5e9f0829565e5432803cee4487e9268f63b4c506
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2025-05-01 15:04:49 -0400

    feat: add Search component to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
 import "./index.css";
 import MovieCard from "../../components/MovieCard/MovieCard";
+import Search from "../../components/Search/Search";
 
 const Home = () => {
   return (
     <div className="containerHome">
+      <div className="containerSearch">
+        <Search />
+      </div>
       <div className="containerMovieCard">
         <MovieCard />
         <MovieCard />
