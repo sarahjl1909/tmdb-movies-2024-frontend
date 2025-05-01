aab004d50b39b3f52d838091c760f36f0907ed3d
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2025-05-01 15:03:39 -0400

    feat: add back button to MovieDetails page

--- src/components/MovieCardDetail/MovieCardDetail.tsx
+++ src/components/MovieCardDetail/MovieCardDetail.tsx
@@ -1,48 +1,89 @@
-import './index.css'
+import "./index.css";
 
 interface props {
-    img: string,
-    title: string,
-    releaseDate: string
-    runtime: string
-    genre: string
-    language: string
-    voteAverage: number
-    overview: string  
+  img: string;
+  title: string;
+  releaseDate: string;
+  runtime: string;
+  genre: string;
+  language: string;
+  voteAverage: number;
+  overview: string;
 }
 
 //function MovieCardDetail({img, title, releaseDate, runtime, genre, language, voteAverage, overview} : props){
- 
 
-function MovieCardDetail(){
+function MovieCardDetail() {
+  const imga =
+    "https://image.tmdb.org/t/p/w185/9Rj8l6gElLpRL7Kj17iZhrT5Zuw.jpg";
 
-    const imga = "https://image.tmdb.org/t/p/w185/9Rj8l6gElLpRL7Kj17iZhrT5Zuw.jpg"
+  return (
+    <div className="containerMovieCardDetail">
+      <div className="movieCardDetail">
+        <img src={imga} alt="image film"></img>
 
-    return(
-            <div className="movieCardDetail">
-                <img src={imga} alt="image film"></img>
-                
-                <div className="movieDetail">
-                    <h3 className="movieTitleDetail">Title of the movie</h3>
-                    <p className="movieInfoDetail">release date | runtime | genre</p>
-                    <p>Language origin</p>
+        <div className="movieDetail">
+          <h3 className="movieTitleDetail">Title of the movie</h3>
+          <p className="movieInfoDetail">release date | runtime | genre</p>
+          <p>Language origin</p>
 
-                    <div className="movieVotesDetail">
-                        <h4>Rating by viewrs</h4>
-                        <p>5</p>
-                        
-                    </div>
+          <div className="movieVotesDetail">
+            <h4>Rating by viewrs</h4>
+            <p>5</p>
+          </div>
 
-                    <p className="movieOverviewDetail">Brief description or summary of the movie.</p>
-                    <button>Ver mais</button>
-                    
-                </div>
-                
-                
-            </div>
-       
-        
-    );
+          <p className="movieOverviewDetail">
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+            Brief description or summary of the movie. Brief description or
+            summary of the movie. Brief description or summary of the movie.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MovieCardDetail
+export default MovieCardDetail;
--- src/pages/MovieDetail/MovieDetail.tsx
+++ src/pages/MovieDetail/MovieDetail.tsx
@@ -1,9 +1,18 @@
-
+import "./index.css";
+import MovieCardDetail from "../../components/MovieCardDetail/MovieCardDetail";
+import MovieCard from "../../components/MovieCard/MovieCard";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const MovieDetail = () => {
-    return (
-        <h1>Movie Detail</h1>
-    );
-}
+  return (
+    <div>
+      <Link to="/" className="link">
+        <IoIosArrowRoundBack className="arrowBack" size={50} color="black" />
+      </Link>
+      <MovieCardDetail />
+    </div>
+  );
+};
 
 export default MovieDetail;
