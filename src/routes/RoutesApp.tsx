943754ffe0ceaa5aa67a7930b1644f3f253d67ca
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2025-05-02 17:32:09 -0400

    fix: correct routing behavior

--- src/routes/RoutesApp.tsx
+++ src/routes/RoutesApp.tsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
-//import MovieDetail from '../pages/MovieDetail/MovieDetail'
+import MovieDetail from "../pages/MovieDetail/MovieDetail";
 import About from "../pages/About/About";
 import NotFound from "../pages/NotFound/NotFound";
+import SearchPage from "../pages/SearchPage/SearchPage";
 
 function RoutesApp() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/movies" element={<NotFound />} />
+      <Route path="/movies/:id" element={<MovieDetail />} />
+      <Route path="/search" element={<SearchPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
