dba4d8d7eedf34da0599e8754349a8b27aa92fd5
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2025-05-02 20:20:18 -0400

    feat: add page about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,7 +1,20 @@
 import "./index.css";
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div className="container">
+      <div className="containerText">
+        <p>
+          This project was developed as part of a technical challenge. The main
+          goal was to build a web application that would display a catalog of
+          movies, allowing users to browse, search, and view detailed
+          information about each movie. The application was developed using
+          React for the frontend and with The Movie Database (TMDB) dataset to
+          retrieve movie data using the Flask API.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
